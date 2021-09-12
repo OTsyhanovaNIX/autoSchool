d6faa35fda6ce6edbdc645ed5b0881af69e013b6
otsyhanova <olha.gorelko7@gmail.com>
2021-09-12 17:36:34 +0300

    HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
         public static void main(String [] args){
             System.out.println("Hello world!");
+            printHelloName();
+        }
+
+        public static void printHelloName(){
+            Scanner name = new Scanner(System.in);
+            System.out.println("Enter your name");
+            String string = name.nextLine();
+            System.out.println("Hello, "+string);
+
         }
 }
